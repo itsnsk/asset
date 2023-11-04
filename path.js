0e798296f8da8b57ae5781780011433f41406457
itsnsk <149897505+itsnsk@users.noreply.github.com>
2023-11-04 23:42:37 +0530

    Add files via upload

---
+++ path.js
@@ -0,0 +1,16 @@
+ <script>
+                  document.getElementById('open-multiple-pages').addEventListener('click', function(event) {
+                      event.preventDefault();
+                      
+                      var urlsToOpen = [
+                          'C:\Users\Sai Kiran\Desktop\practice\Append\assets\form.html ',
+                          'C:\Users\Sai Kiran\Desktop\practice\Append\assets\faculty.html ',
+                          'urls u wanted'
+                      ];
+          
+                      urlsToOpen.forEach(function(C:\Users\Sai Kiran\Desktop\practice\Append\assets\form.html) {
+                          window.open(C:\Users\Sai Kiran\Desktop\practice\Append\assets\faculty.html, '_blank'); 
+                      });
+                  });
+              </script>
+          
